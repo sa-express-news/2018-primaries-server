ceeb0e67f56103dd02086f275e63d27a81ee796f
Kia Farhang <kfarhang0@gmail.com>
2018-02-12 12:04:59 -0600

    Get most of the way through refactoring Google Sheets quick start into a group of functions that will return sheets data

---
+++ src/google-sheets/index.ts
@@ -0,0 +1,171 @@
+
+import * as fs from "fs";
+import * as readlineSync from "readline-sync";
+import { promisify } from 'util';
+const google = require("googleapis");
+const googleAuth = require("google-auth-library");
+import { GoogleCredentials } from '../types';
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+
+const authorize = async (credentials: GoogleCredentials): Promise<any> => {
+    const clientSecret = credentials.installed.client_secret;
+    const clientId = credentials.installed.client_id;
+    const redirectUrl = credentials.installed.redirect_uris[0];
+    const auth = new googleAuth();
+    const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+
+    try {
+        const token = await readFile(TOKEN_PATH);
+        oauth2Client.credentials = JSON.parse(token.toString());
+        return oauth2Client;
+    } catch (e) {
+        return getNewToken(oauth2Client);
+    }
+}
+
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+
+const getNewToken = async (oauth2Client: any): Promise<any> => {
+    const authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+
+    console.log('Authorize this app by visiting this url: ', authUrl);
+
+    // const rl = readline.createInterface({
+    //     input: process.stdin,
+    //     output: process.stdout
+    // });
+
+    const code = readlineSync.question("Enter the code from that page here: ");
+
+    try {
+        const token = await oauth2Client.getToken(code);
+        oauth2Client.credentials = token;
+        await storeToken(token);
+        return token;
+    } catch (error) {
+        console.log(`Error while trying to receive access token: ${error}`);
+    }
+
+    // rl.question('Enter the code from that page here: ', function (code) {
+    //     rl.close();
+    //     oauth2Client.getToken(code, async function (err: Error, token: any) {
+    //         if (err) {
+    //             console.log('Error while trying to retrieve access token', err);
+    //             return;
+    //         }
+    //         oauth2Client.credentials = token;
+    //         await storeToken(token);
+    //         listMajors(oauth2Client);
+    //     });
+    // });
+
+}
+
+
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+
+const storeToken = async (token: object): Promise<void> => {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
+    }
+
+    try {
+        await writeFile(TOKEN_PATH, JSON.stringify(token));
+        console.log(`Token stored to ${TOKEN_PATH}`);
+    } catch (error) {
+        console.log(`Error storing token: ${error}`);
+    }
+}
+
+
+// Load client secrets from a local file.
+const loadGoogleCredentials = async (): Promise<GoogleCredentials> => {
+    try {
+        const rawCredentials = await readFile('client_secret.json');
+        const credentials: GoogleCredentials = JSON.parse(rawCredentials.toString());
+        return credentials;
+    } catch (e) {
+        throw new Error(`Error loading credentials file: ${e}`);
+    }
+}
+
+
+
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+
+const fetchData = async (auth: any): Promise<object> => {
+    const sheets = google.sheets('v4');
+
+    const get = promisify(sheets.spreadsheets.values.get);
+    try {
+        const data: object = await get({
+            auth: auth,
+            spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+            range: 'Class Data!A2:E',
+        });
+
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const main = async (): Promise<void> => {
+    try {
+        const credentials = await loadGoogleCredentials();
+        const oAuthClient = await authorize(credentials);
+        const data = await fetchData(oAuthClient);
+        console.log(data);
+
+
+    } catch (error) {
+        console.log(`Error fetching data from Google Sheets: ${error}`);
+    }
+}
+
+main();
