7337821a22609bbd9a81dd9b7939e7f556426eb0
Kia Farhang <kfarhang0@gmail.com>
2018-02-19 13:33:56 -0600

    Add interfaces for Primary, Race and Candidate data structures

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,5 +7,24 @@ export interface GoogleCredentials {
         auth_provider_x509_cert_url: string;
         client_secret: string;
         redirect_uris: string[];
-    }
+    };
+}
+
+export interface Primary {
+    title: string;
+    id: number;
+    races: Race[];
+}
+
+export interface Race {
+    isRepublican: boolean;
+    title: string;
+    candidates: Candidate[];
+    source: string;
+    source_url: string;
+}
+
+export interface Candidate {
+    name: string;
+    votes: number;
 }
