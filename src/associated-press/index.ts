3ee065499e2b9e5a23fd0f0462e7e6d6246ea856
Kia Farhang <kfarhang0@gmail.com>
2018-03-03 15:50:45 -0600

    Finish code + tests for a function to extract candidates from AP data structure

--- src/associated-press/index.ts
+++ src/associated-press/index.ts
@@ -1,12 +1,21 @@
-import {APCandidate, Candidate} from "../types";
+import { APCandidate, Candidate } from "../types";
 
 export const extractCandidates = (candidates: APCandidate[]): Candidate[] => {
     const extractedCandidates: Candidate[] = [];
     candidates.forEach((candidate: APCandidate) => {
-        extractedCandidates.push({
+        const theCandidate: Candidate = {
             name: `${candidate.first} ${candidate.last}`,
             votes: candidate.voteCount ? candidate.voteCount : 0,
-        });
+        };
+
+        if (candidate.incumbent === true) {
+            theCandidate.incumbent = true;
+        }
+        if (candidate.winner === "X") {
+            theCandidate.winner = true;
+        }
+
+        extractedCandidates.push(theCandidate);
     });
 
     return extractedCandidates;
--- src/associated-press/spec.ts
+++ src/associated-press/spec.ts
@@ -14,6 +14,7 @@ describe("Associated Press", () => {
                     ballotOrder: 4,
                     polNum: "51927",
                     voteCount: 10,
+                    incumbent: true,
                 },
                 {
                     first: "Cedric",
@@ -33,6 +34,7 @@ describe("Associated Press", () => {
                     ballotOrder: 6,
                     polNum: "51930",
                     voteCount: 30,
+                    winner: "X",
                 },
                 {
                     first: "Tom",
@@ -77,11 +79,33 @@ describe("Associated Press", () => {
                     }
                 });
             });
-            it("if a parameter object lacks a 'voteCount' property, the return objects votes will be 0", () => {
+            it("if a parameter object lacks a 'voteCount' property, the return objec't's votes will be 0", () => {
                 results.forEach((candidate, index) => {
                     if (!mockData[index].voteCount) {
                         assert.strictEqual(candidate.votes, 0);
                     }
+                });
+            });
+            it("if the parameter object has an 'incumbent' property set to true, return object will have same", () => {
+                const mockIncumbent = results[0];
+                assert.property(mockIncumbent, "incumbent");
+                assert.isTrue(mockIncumbent.incumbent);
+            });
+            it("if parameter object lacks 'incumbent' property, return object will lack same", () => {
+                const mockChallengers = results.slice(1);
+                mockChallengers.forEach((challenger) => {
+                    assert.notProperty(challenger, "incumbent");
+                });
+            });
+            it("if param object has 'winner' property set to 'X', return object will have 'winner' = true", () => {
+                const mockWinner = results[2];
+                assert.property(mockWinner, "winner");
+                assert.isTrue(mockWinner.winner);
+            });
+            it("if param object lacks 'winner' property, return object will lack same", () => {
+                const mockLosers = [results[0], results[1], results[3]];
+                mockLosers.forEach((loser) => {
+                    assert.notProperty(loser, "winner");
                 });
             });
         });
