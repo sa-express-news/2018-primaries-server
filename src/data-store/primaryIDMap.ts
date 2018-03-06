fe4d76d57a45c3d58c3d96a2a3920e89add2d8e0
Kia Farhang <kfarhang0@gmail.com>
2018-03-06 17:24:12 -0600

    Create a map of all primaries to assign arbitrary IDs for front-end consumption

---
+++ src/data-store/primaryIDMap.ts
@@ -0,0 +1,23 @@
+const primaryIDMap = new Map();
+
+primaryIDMap.set("Governor", 0);
+primaryIDMap.set("U.S. Senate", 1);
+primaryIDMap.set("U.S. House - District 21", 2);
+primaryIDMap.set("U.S. House - District 23", 3);
+primaryIDMap.set("Land Commissioner", 4);
+primaryIDMap.set("U.S. House - District 35", 5);
+primaryIDMap.set("Lieutenant Governor", 6);
+primaryIDMap.set("Agriculture Commissioner", 7);
+primaryIDMap.set("Railroad Commissioner", 8);
+primaryIDMap.set("Bexar County District Attorney", 9);
+primaryIDMap.set("District Clerk", 10);
+primaryIDMap.set("County Clerk", 11);
+primaryIDMap.set("County Commissioner Pct. 2", 12);
+primaryIDMap.set("County Probate Court-at-Law No 1 Judge", 13);
+primaryIDMap.set("County Chair", 14);
+primaryIDMap.set("State Representative - District 121", 15);
+primaryIDMap.set("State Representative - District 122", 16);
+primaryIDMap.set("State Representative - District 116", 17);
+primaryIDMap.set("State Representative - District 117", 18);
+primaryIDMap.set("State Representative - District 118", 19);
+primaryIDMap.set("State Representative - District 124", 20);
