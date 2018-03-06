651ad6c8134689a4bb6bc56d3f77026d9faff22e
Kia Farhang <kfarhang0@gmail.com>
2018-03-06 17:42:10 -0600

    Update a typo in primary ID map

--- src/data-store/primaryIDMap.ts
+++ src/data-store/primaryIDMap.ts
@@ -13,7 +13,7 @@ primaryIDMap.set("Bexar County District Attorney", 9);
 primaryIDMap.set("District Clerk", 10);
 primaryIDMap.set("County Clerk", 11);
 primaryIDMap.set("County Commissioner Pct. 2", 12);
-primaryIDMap.set("County Probate Court-at-Lw No 1 Judge", 13);
+primaryIDMap.set("County Probate Court-at-Law No 1 Judge", 13);
 primaryIDMap.set("County Chair", 14);
 primaryIDMap.set("State Representative - District 121", 15);
 primaryIDMap.set("State Representative - District 122", 16);
