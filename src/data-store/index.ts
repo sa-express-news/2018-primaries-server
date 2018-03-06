f20982221e5da0603cc64daf9e8980e057dd4ea0
Kia Farhang <kfarhang0@gmail.com>
2018-03-06 14:55:08 -0600

    Add a function to update the application data store

---
+++ src/data-store/index.ts
@@ -0,0 +1,43 @@
+import * as ap from "../associated-press";
+import * as google from "../google-sheets";
+import { DataStore } from "../types";
+import * as util from "../util";
+
+export const dataStore: DataStore = {
+    primaries: [],
+    nextAPRequestURL: process.env.AP_URL as string,
+};
+
+export const generateDataStore = async (previousDataStore: DataStore): Promise<DataStore> => {
+    try {
+        const apData = await ap.fetchAPData(previousDataStore.nextAPRequestURL);
+        const { primaries: apPrimaries, nextURL } = apData;
+        const googleData: string[][] = await google.fetchGoogleSheetData(process.env.SPREADSHEET_ID as string, "Election Data!A2:E");
+        const googlePrimaries = google.buildPrimaries(googleData);
+
+        const mergedPrimaries = apPrimaries.concat(googlePrimaries);
+
+        return {
+            primaries: util.mergeAndUpdateArraysOfObjects(previousDataStore.primaries, mergedPrimaries, "title"),
+            nextAPRequestURL: nextURL,
+        };
+    } catch (error) {
+        throw error;
+    }
+};
+
+// export const updateDataStore = async (): Promise<void> => {
+
+//     try {
+//         const apData = await ap.fetchAPData(dataStore.nextAPRequestURL);
+//     }
+
+//     // const updateAPData = async (): Promise<void> => {
+//     //     const apData = await ap.fetchAPData(dataStore.nextAPRequestURL);
+//     //     const { primaries, nextURL } = apData;
+//     //     dataStore.nextAPRequestURL = nextURL;
+//     //     if (primaries.length > 0) {
+//     //         dataStore.APPrimaries = util.mergeAndUpdateArraysOfObjects(dataStore.APPrimaries, primaries, "title");
+//     //     }
+//     // };
+// }
